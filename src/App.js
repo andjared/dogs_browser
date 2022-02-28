0e6b9eb352cbcbd4f24987c0f7cc2fa11c2c3c12
andja <andja.velimirovic@gmail.com>
2022-02-28 12:15:48 +0100

    fetch random dog image onclick

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import List from "./components/List";
 
 function App() {
   const [breedsList, setBreedsList] = useState(null);
+  const [dogImg, setDogImg] = useState(null);
 
   useEffect(() => {
     const getList = async () => {
@@ -16,9 +17,18 @@ function App() {
     };
     getList();
   }, []);
+  const getBreedImage = async (breed) => {
+    const response = await fetch(
+      `https://dog.ceo/api/breed/${breed}/images/random`
+    );
+    const data = await response.json();
+    setDogImg(data.message);
+    console.log(dogImg);
+  };
   return (
     <div className="container">
-      <List breedsList={breedsList} />
+      <List breedsList={breedsList} handleClick={getBreedImage} />
+      {dogImg && <img alt="dogImg" src={dogImg}></img>}
     </div>
   );
 }
--- src/components/List.js
+++ src/components/List.js
@@ -1,10 +1,14 @@
 import React from "react";
 
-export default function List({ breedsList }) {
+export default function List({ breedsList, handleClick }) {
   return (
     <div className="select">
       <label htmlFor="breeds">Choose a breed</label>
-      <select name="breeds" id="breeds">
+      <select
+        name="breeds"
+        id="breeds"
+        onClick={(e) => handleClick(e.target.value)}
+      >
         {breedsList &&
           breedsList.map((breed) => <option key={breed}>{breed}</option>)}
       </select>
